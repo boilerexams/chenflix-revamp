fc1336523b49e525710dee3ac38450e55b932e8a
Lawrence Smith <106993743+leleuns@users.noreply.github.com>
2023-10-08 00:39:40 -0400

    capy position and other tings

--- src/scripts/webgl/TCanvas.ts
+++ src/scripts/webgl/TCanvas.ts
@@ -6,7 +6,6 @@ import { controls } from './utils/OrbitControls'
 import vertexShader from './shader/vs.glsl'
 import fragmentShader from './shader/fs.glsl'
 import { calcCoveredTextureScale } from './utils/coveredTexture'
-console.log('helloe')
 export class TCanvas {
   private assets: Assets = {
     model: { path: 'capybara_low_poly/scene.gltf' },
@@ -23,8 +22,8 @@ export class TCanvas {
 
   private init() {
     gl.setup(this.container)
-     gl.scene.background = new THREE.Color('black')
-    gl.camera.position.z = 1.5
+    gl.scene.background = new THREE.Color('black')
+    gl.camera.position.set(2.5, 0.5, 0);
   }
 
   private createObjects() {
@@ -35,7 +34,7 @@ export class TCanvas {
       
     // You can adjust the position, rotation, and scale of the model here
     model.position.set(0, -0.3, 0)
-    model.rotation.set(0, 1, 0)
+    model.rotation.set(0, 2, 0)
     model.scale.set(1, 1, 1)
 
     const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32); // Reduced size from 20 to 10
@@ -50,7 +49,7 @@ export class TCanvas {
     model.add(groundMesh);
    
     const spotLight = new THREE.SpotLight(0xffffff,  5, 100, 0.22, 1);
-    spotLight.position.set(0, 10, 0);
+    spotLight.position.set(0, 5, 0);
     spotLight.castShadow = true;
     // spotLight.shadow.bias = -0.0001;
     model.add(spotLight);
