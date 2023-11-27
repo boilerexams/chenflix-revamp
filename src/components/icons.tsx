120d57489f698aaee8d64c8f3627fa4f76c5a21d
Lawrence Smith <106993743+leleuns@users.noreply.github.com>
2023-11-27 11:19:56 -0500

    more css bruh

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,7 +12,7 @@ export function CourseCard({courseAbbreviation, courseName, courseDescr, semeste
 			>
 				{/* px-5 ? */}
 				<div
-					className="relative flex aspect-[2.5/2] select-none flex-col items-center justify-center gap-5 rounded-3xl text-center font-bold text-white opacity-100 transition-all duration-300 group-hover:opacity-0"
+					className="relative flex aspect-[2.5/2] select-none flex-col items-center justify-center gap-2 rounded-3xl text-center font-bold text-white opacity-100 transition-all duration-300 group-hover:opacity-0 px-1"
 				>
 					<div
 						className="absolute left-0 top-0 h-full w-full rounded-3xl"
@@ -27,10 +27,10 @@ export function CourseCard({courseAbbreviation, courseName, courseDescr, semeste
 						}}
 					></div>
 					<h1 className="z-10 text-3xl">{courseName}</h1>
-					<h2 className="z-10 max-w-[90%]opacity-75 text-sm ">{courseDescr}</h2>
+					<h2 className="z-10 max-w-[90%]opacity-75 text-xs ">{courseDescr}</h2>
 				</div>
 				<div className="absolute left-0 top-0 grid aspect-[2.5/2] h-full w-full select-none place-items-center rounded-3xl text-center opacity-0 transition-all duration-300 group-hover:opacity-100">
-					<div className="text-white place-items-center text-3xl  hover:cursor-pointer font-bold">
+					<div className="text-white place-items-center text-2xl  hover:cursor-pointer font-bold">
 						{semesterList.map((semester, index) => (
 							<p key={index} style={{margin: "0.5rem 0"}}>
 								<a href={semesterLinks[index]} className= 'flex h-full w-full flex-col items-center justify-center rounded-tr-3xl text-center text-white transition-all duration-150 md:hover:text-black'>
